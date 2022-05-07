5cbb36270cbcafde7c895d40fa3c201427c235f6
ju-resplande <julianarsg13@gmail.com>
2022-05-07 01:33:20 +0000

    task 2 submission

--- submission.py
+++ submission.py
@@ -2,6 +2,7 @@ import json
 import os
 
 import pandas as pd
+from scipy.stats import mode
 
 prediction_dir = "predictions"
 test_data = "data/test_task2.csv"
@@ -10,17 +11,17 @@ ids = pd.read_csv(test_data, sep=";")["id"]
 
 predictions = list()
 for experiment_dir in os.listdir(prediction_dir):
-    prediction_file = os.path.append(prediction_dir, experiment_dir, "predictions.json")
+    prediction_file = os.path.join(prediction_dir, experiment_dir, "predictions.json")
 
     with open(prediction_file) as f:
         prediction = json.load(f)
 
-    predictions.extend(prediction)
+    predictions.append(prediction)
 
-ensemble = pd.DataFrame(predictions).map({"negativo": -1, "neutro": 0, "positivo": 1})
-ensemble = ensemble.mode()
-
-submission = pd.concat([ids, ensemble], axis=0)
+prediction_map = {"negativo": -1, "neutro": 0, "positivo": 1}
+ensemble = pd.DataFrame(predictions).applymap(lambda cell: prediction_map[cell])
+ensemble = pd.Series(mode(ensemble.values).mode[0])
+submission = pd.concat([ids, ensemble], axis=1)
 submission.to_csv("DeepLearningBrasil_task2.csv", sep=";", index=None, header=None)
 
 
